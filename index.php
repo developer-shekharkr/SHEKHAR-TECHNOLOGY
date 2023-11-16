b29cad713849a3334bb52bd81b6adb71b9f801e3
Shekhar Kumar <74418616+developer-shekharkr@users.noreply.github.com>
2023-11-16 09:34:37 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+    <!-- header -->
+    <div class="header">
+        <div class="left"><img src="./SHEKHAR.png" alt="logo"></div>
+        <div class="right">
+            <b>+91 9876543210</b>
+            <p>All Days 9:00 AM to 18:00 PM</p>
+        </div>
+
+    </div>
+
+    <!-- nav bar -->
+    <nav class="navbar navbar-expand-sm  navbar-dark">
+        <div class="leftnav">
+            <ul>
+                <li><span><a href="#">HOME</a></span></li>
+                <li><a href="#">ABOUT US</a></li>
+                <li><a href="#">SERVICES</a></li>
+                <li><a href="#">CONTACT</a></li>
+                <li><a href="#">CAREER</a></li>
+                <li><a href="#">BLOG</a></li>
+            </ul>
+        </div>
+
+        <div class="rightnav">
+            <div><a href="#"><img src="./icons8-facebook-50.png"></a></div>
+            <div><a href="#"><img src="./icons8-instagram-50.png"></a></div>
+            <div><a href="#"><img src="./icons8-linkedin-50.png"></a></div>
+            <div><a href="#"><img src="./icons8-twitter-50.png"></a></div>
+        </div>
+    </nav>
+
+
+    <!-- banner -->
+    <div class="banner">
+        <h2>High Quality <span>Semiconductor Design </span>And Services</h2>
+        <p>Bhive Design is a semiconductor IC design services company headquartered at Bangalore and design center at Ranchi. We provide IC design services to the semiconductor industries in India and abroad.
+
+            Request For Design</p>
+        <button>Request For Design</button>
+    </div>
+
+<!-- floting 4  divs -->
+    <div class="box">
+        <div style="background-color: white;" class="div">
+            <h3 style="color: black;">IC Chip Design & Services</h3>
+            <p style="color: black;">System-level design and microarchitecture planning</p>
+        </div>
+
+        <div style="background-color:rgb(142, 20, 20);" class="div">
+            <h3>Analog / RF, mm Wave Custom Layout</h3>
+            <p>Automated design platforms for complex ICs</p>
+        </div>
+        <div style="background-color:white;" class="div">
+            <h3 style="color: black;">Physical Design (Netlist to GDS)</h3>
+            <p style="color: black;">Physical designers are responsible for producing high quality design tapeout</p>
+        </div>
+        <div style="background-color: rgb(142, 20, 20);" class="div">
+            <h3>ESD Memory layout development</h3>
+            <p>System Level ESD Analysis of Memory Devices</p>
+        </div>
+    </div>
+
+<!-- iso certified -->
+    <div class="certified">
+        <div class=" rounded">
+            <img src="./semi-conductor-ip-b6875e59.jpg">
+            </div>
+        <div class="right">
+            <img src="./ISO-certified-2-1024x278.png">
+            <p>Bhive Design is a semiconductor IC design services company headquartered at Bangalore and design center at Ranchi. We provide IC design services to the semiconductor industries in India and abroad.
+
+Bhive Design team has vast experience in Digital front-end Design & Verification, Analog / RF, mm Wave Custom Layout, Standard cells. iOS, ESD Memory layout development, Physical Design, Automation in Perl & Skill, Embedded & Firmware.</p>
+<button class="readmore">Read More</button>   
+        </div>
+       
+    </div>
+
+    <!-- our industry -->
+    <div class="industry">
+        <h3>Our Industry <span>work</span></h3>
+        <hr style="background-color: white;" id="hr">
+        <p>
+We Design with following Tools
+
+</p>
+    </div>
+
+    <!-- 5 logos -->
+    <div class="logos">
+        <div class="img">
+            <img src="./plan.png"><br>
+            <b>Military / Aerospace</b>
+        </div>
+        <div class="img">  <img src="./antena.png"><br>
+            <b>
+Communication</b></div>
+        <div class="img">  <img src="./automation-setting.png"><br>
+            <b>Automation</b></div>
+        <div class="img">  <img src="./consumers-electronics.png"><br>
+            <b>Consumer Electronic</b></div>
+        <div class="img">  <img src="./medical_science-1.png"><br>
+            <b>Medical Science</b></div>
+    </div>
+
+
+<!-- office img with 3 divs -->
+<div class="office">
+    <div class="top-reasons"><h2>Top reasons to work with us</h22><br>
+<p >Some of Our Detailed Service Offerings.</p></div>
+
+<div class="ofcimg">
+    <div class="boxes">
+        <img src="./3div1.jpg"><br>
+        <b>Efficiency</b>
+        <p>Our designs are simple, uses minimum number of components and very cost-effective to manufacture. We always choose parts that are widely available across the world & when we design a product.</p>
+    </div>
+    <div class="boxes">
+        <img src="./3div2.png"><br>
+        <b>Speed</b>
+        <p>We take projects only up to our handling capacity and hence we are able to concentrate on each project & finish them within reasonable time</p>
+    </div>
+    <div class="boxes">
+        <img src="./3div3.png"><br>
+        <b>Designs</b>
+        <p>All the designs will include a working prototype which is assembled in our lab and fully tested before dispatching them to you.</p>
+    </div>
+    </div>
+
+</div>
+
+<!-- clients -->
+
+<div class="industry">
+        <h3>Our Prestigious <span>clients</span></h3>
+        <hr id="hr">
+        <p>
+        we design products for the following customers
+
+</p>
+    </div>
+
+    <!-- brands -->
+    <marquee>
+    <div class="brands">
+        
+        <div class="brand-logo"><img src="./qualcomm.png"></div>
+        <div class="brand-logo"><img src="./nvidia.png"></div>
+        <div class="brand-logo"><img src="./ibm.png"></div>
+        <div class="brand-logo"><img src="./graphics1.png"></div>
+        <div class="brand-logo"><img src="./ibm.png"></div>
+        <div class="brand-logo"><img src="./nvidia.png"></div>
+        <div class="brand-logo"><img src="./graphics1.png"></div>
+        <div class="brand-logo"><img src="./nvidia.png"></div>
+        <div class="brand-logo"><img src="./ibm.png"></div>
+        <div class="brand-logo"><img src="./nvidia.png"></div>
+        <div class="brand-logo"><img src="./graphics1.png"></div>
+        <div class="brand-logo"><img src="./nvidia.png"></div>
+        <div class="brand-logo"><img src="./nvidia.png"></div>
+        <div class="brand-logo"><img src="./graphics1.png"></div>
+        <div class="brand-logo"><img src="./nvidia.png"></div>
+        
+    </div>
+    </marquee>
+
+    <!-- articles -->
+
+    <div class="articles">
+<h2 style="color: rgb(142, 20, 20) ;">Our Latest Article</h2>
+<hr style="background-color:rgb(142, 20, 20);" id="hr">
+<div class="cards">
+<div class="artcards">
+    <img src="./articles1.jpg"><br>
+    <b>Emerging Trends in Semiconductor Chip Design Business</b><br>
+    <span><b>Read More</b></span>
+</div>
+<div class="artcards">
+    <img src="./articles2.jpg"><br>
+    <b>The Future of Semiconductor Chip Design: Trends and Prediction</b><br>
+    <span><b>Read More</b></span></div>
+<div class="artcards">
+    <img src="./articles3.jpg"><br>
+    <b>Building a Sustainable Business Model in the Semiconductor Chip Design Industry</b><br>
+    <span><b>Read More</b></span></div>
+<div class="artcards">
+    <img src="./articles4.jpg"><br>
+    <b>How to Stay Ahead of the Competition in the Semiconductor Chip Design Industry</b><br>
+    <span><b>Read More</b></span></div>
+    </div>
+<button><b>Read More</b></button>
+    </div>
+
+    <!-- Customer Recommendations -->
+<div class="customer">
+    <div class="topcustomer"><h2>Customer Recommendations & <span>Testimonials</span></h2>
+<hr id="hr">
+<p style="color: black;">See what our customers are saying!</p></div>
+
+<div class="reviewsmain">
+<div class="reviews"><i>
+"Bhive Design exceeded my expectations with their chip design services. They were able to take my idea and turn it into a functional and efficient design that exceeded my expectations. I highly recommend their services to anyone looking for high-quality chip design.”</i>
+<div class="profile">
+<img src="./reviewPRINCE-KUMAR-1.png">
+<b style="color: grey;">PRINCE KR.</b><br>
+<b class="co">CO-FOUNDER</b>
+</div>
+</div>
+
+<div class="reviews"><i>
+"Bhive Design exceeded my expectations with their chip design services. They were able to take my idea and turn it into a functional and efficient design that exceeded my expectations. I highly recommend their services to anyone looking for high-quality chip design.”</i>
+<div class="profile">
+<img src="./review SHIVA-ORAON-2.png">
+<b style="color: grey;">SHEKHAR KR.</b><br>
+<b class="co">DIRECTOR</b>
+</div>
+</div>
+
+
+<div class="reviews"><i>
+"Bhive Design exceeded my expectations with their chip design services. They were able to take my idea and turn it into a functional and efficient design that exceeded my expectations. I highly recommend their services to anyone looking for high-quality chip design.”</i>
+<div class="profile">
+<img src="./reviewVISHAL-SINGH-3.png">
+<b style="color: grey;">ANAND KR.</b><br>
+<b class="co">MANAGEMENT DIRECTOR</b>
+</div>
+</div>
+</div>
+
+</div>
+
+</body>
+
+</html>
+
+
+
+<!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
